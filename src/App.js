30561975a568a957a2be6b6fd566dc434ccba953
ktw1016 <ktw_1016@hotmail.com>
2021-02-03 22:36:37 -0500

    simple todo list app to use hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import _ from "lodash";
+import "./App.css";
 
-function App() {
+
+const App = () => {
+  const [todoList, setTodoList] = useState([{key: "defaultItem", value: "An example of TODO item!"}]);
+  const [newTodoItemText, setTodoItemText] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div style={{display: "flex", flexDirection: "column"}}>
+        <div style={{padding: "50px 0px 50px"}}>
+          <input type="text" value={newTodoItemText} onChange={(evt) => setTodoItemText(evt.target.value)} placeholder="Call mom at 12" />
+          <button onClick={() => setTodoList([
+            ...todoList,
+            {
+              key: `${newTodoItemText}${todoList.length}`,
+              value: newTodoItemText,
+            }
+          ])}>
+            Add!
+          </button>
+        </div>
+        <div>
+          {todoList.map(({ value, key }) =>
+            <div key={key} className="todo-item">
+              {value}
+              <button
+                id={key}
+                style={{ marginLeft: 10 }}
+                onClick={(evt) => setTodoList(
+                  _.reject(todoList, ({key}) => evt.target.id === key)
+                )}
+              >
+                Remove
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
